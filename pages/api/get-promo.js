f7fa6462750da20ead62b9b13291992cdc3b763a
gnlaures <gn.laureano@gmail.com>
2021-08-22 16:15:37 -0300

    Fix - fromBase64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,9 +1,9 @@
 import  { GoogleSpreadsheet } from 'google-spreadsheet'
 
-const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID);
+const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 const fromBase64 = value => {
-    const buff = new Buffer.from(value, 'base64');
+    const buff = Buffer.from(value, 'base64');
     return buff.toString('ascii');
 }
 
